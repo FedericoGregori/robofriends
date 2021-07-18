f0e41467a7c1695b064b978fb0ca2487e33922d3
Federico Gregori <jfedericogregori@gmail.com>
2021-07-18 19:18:14 -0300

    First commit: Building A React App 2 Finished

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import { robots } from './robots'
+import CardList from './CardList';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello greeting={"Hello" + "React Ninja"}/>
+    <CardList robots={robots} />
   </React.StrictMode>,
   document.getElementById('root')
 );
